665cce5f4210b310933e8f0b83d3b86f421a0bd1
whdudgms <whdudgms123@naver.com>
2024-07-17 16:51:40 +0900

    projectcreate

---
+++ GoodsDAO.java
@@ -0,0 +1,10 @@
+class GoodsDAO
+{
+
+	public void insert(){
+		System.out.println("goods insert");
+	}
+	public void update(){
+		System.out.println("goods update");
+	}
+}
---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello
+{
+		public static void main(String[] args){
+			GoodsDAO dao = new GoodsDAO();
+			dao.insert();
+			dao.update();
+		}
+}
